51e9dd6592a7176e09969b5d4e9fc4bb9823d983
Mike <mike@t2.ca>
2020-04-16 13:00:19 -0600

    fix identity-context

--- packages/www/src/pages/app.js
+++ packages/www/src/pages/app.js
@@ -1,35 +1,35 @@
-import React, { useContext } from 'react';
-import { Heading, Button, Flex } from 'theme-ui';
-import { Router } from '@reach/router';
-import { IdentityContext } from '../../identity-context';
+// import React, { useContext } from 'react';
+// import { Heading, Button, Flex } from 'theme-ui';
+// import { Router } from '@reach/router';
+// import { IdentityContext } from '../../identity-context';
 
-let Dash = () => {
-  const { user } = useContext(IdentityContext);
+// let Dash = () => {
+//   const { user } = useContext(IdentityContext);
 
-  return <div>Dash hasUser: {user && user.user_metadata.full_name}</div>;
-};
+//   return <div>Dash hasUser: {user && user.user_metadata.full_name}</div>;
+// };
 
-let DashLoggedOut = (props) => {
-  const { netlifyIdentity } = useContext(IdentityContext);
-  return (
-    <Flex sx={{ flexDirection: 'column', p: 3 }}>
-      <Heading as="h1">Get Stuff Done</Heading>
-      <Button
-        sx={{ mt: 2 }}
-        onClick={() => {
-          netlifyIdentity.open();
-        }}
-      >
-        Button
-      </Button>
-    </Flex>
-  );
-};
+// let DashLoggedOut = (props) => {
+//   const { netlifyIdentity } = useContext(IdentityContext);
+//   return (
+//     <Flex sx={{ flexDirection: 'column', p: 3 }}>
+//       <Heading as="h1">Get Stuff Done</Heading>
+//       <Button
+//         sx={{ mt: 2 }}
+//         onClick={() => {
+//           netlifyIdentity.open();
+//         }}
+//       >
+//         Button
+//       </Button>
+//     </Flex>
+//   );
+// };
 
-export default (props) => {
-  return (
-    <Router>
-      <Dash path="/app" />
-    </Router>
-  );
-};
+// export default (props) => {
+//   return (
+//     <Router>
+//       <Dash path="/app" />
+//     </Router>
+//   );
+// };
