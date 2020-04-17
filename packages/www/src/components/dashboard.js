e016dc4c4848f7c2b3e1a1f7297b793e3bb40b87
Mike <mike@t2.ca>
2020-04-16 21:12:00 -0600

    switch to useReducer in dashboard component

--- packages/www/src/components/dashboard.js
+++ packages/www/src/components/dashboard.js
@@ -1,10 +1,35 @@
-import React, { useContext } from 'react'
-import { Container, Flex, Heading, NavLink, Button } from 'theme-ui'
+import React, { useReducer, useContext, useRef } from 'react'
+import {
+  Container,
+  Flex,
+  Heading,
+  Input,
+  Label,
+  NavLink,
+  Button,
+  Checkbox,
+} from 'theme-ui'
 import { IdentityContext } from '../../identity-context'
 import { Link } from 'gatsby'
 
+const todosReducer = (state, action) => {
+  switch (action.type) {
+    case 'addTodo':
+      return [{ done: false, value: action.payload }, ...state]
+    case 'toggleTodoDone':
+      const newState = [...state]
+      newState[action.payload] = {
+        done: !state[action.payload].done,
+        value: state[action.payload].value,
+      }
+      return newState
+  }
+}
+
 const Dashboard = () => {
   const { user, identity: netlifyIdentity } = useContext(IdentityContext)
+  const [todos, dispatch] = useReducer(todosReducer, [])
+  const inputRef = useRef()
 
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1)
@@ -35,6 +60,30 @@ const Dashboard = () => {
         <div>
           This is top secret data! {user && user.user_metadata.full_name}
         </div>
+        <Flex
+          as="form"
+          onSubmit={(e) => {
+            e.preventDefault()
+            dispatch({ type: 'addTodo', payload: inputRef.current.value })
+            inputRef.current.value = ''
+          }}
+        >
+          <Label sx={{ display: 'flex' }}>
+            <span>Add Todo</span>
+            <Input ref={inputRef} sx={{ ml: 1 }} />
+          </Label>
+          <Button sx={{ ml: 1 }}>submit</Button>
+        </Flex>
+        <Flex sx={{ flexDirection: 'column' }}>
+          <ul sx={{ listStyleType: 'none' }}>
+            {todos.map((todo) => (
+              <Flex as="li" key={todo.value}>
+                <Checkbox checked={todo.done} />
+                <span>{todo.value}</span>
+              </Flex>
+            ))}
+          </ul>
+        </Flex>
       </Flex>
     </Container>
   )
