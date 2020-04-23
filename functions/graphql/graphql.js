bab8b681ec3795f9ecbc3b1be946a4e3303ff270
Mike <mike@t2.ca>
2020-04-23 13:00:50 -0600

    fix graphql.js mistakes

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -27,7 +27,7 @@ const resolvers = {
       if (!user) {
         return [];
       } else {
-        const results = await client.query(q.Paginate(q.Match(q.Index('todos_by_user'))));
+        const results = await client.query(q.Paginate(q.Match(q.Index('todos_by_user'), user)));
         return results.data.map(([ref, text, done]) => ({
           id: ref.id,
           text,
@@ -62,7 +62,7 @@ const resolvers = {
       }
       const results = await client.query(
         q.Update(q.Ref(q.Collection('todos'), id), {
-          date: {
+          data: {
             done: true
           }
         })
