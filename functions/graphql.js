2e32d140e23155f2b906198e36e9da18658bdc25
Mike <mike@t2.ca>
2020-04-29 21:00:31 -0600

    test

--- functions/graphql.js
+++ functions/graphql.js
@@ -48,7 +48,7 @@ const resolvers = {
     //   }
     // },
     getList: async (parent, args, { user }) => {
-      const results = await client.query(q.Paginate(q.Match(q.Index('getList'), user)));
+      const results = await client.query(q.Paginate(q.Match(q.Index('allRegistrations'), user)));
       return console.log(results);
     }
   }
