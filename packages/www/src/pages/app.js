628c901c3bfc42b747b54c0c85e39a1dd3fc5b6b
Mike <mike@t2.ca>
2020-05-07 17:17:12 -0600

    bump versions

--- packages/www/src/pages/app.js
+++ packages/www/src/pages/app.js
@@ -14,7 +14,7 @@ let DashLoggedOut = () => {
         <NavLink as={Link} to="/" p={2}>
           Home
         </NavLink>
-        <NavLink as={Link} to="/app/" p={2}>
+        <NavLink as={Link} to="/app/dashboard/" p={2}>
           Dashboard
         </NavLink>
         {user && (
@@ -43,13 +43,13 @@ const App = () => {
   if (!user) {
     return (
       <Router>
-        <DashLoggedOut path="/app/" />
+        <DashLoggedOut path="/app/dashboard/" />
       </Router>
     )
   }
   return (
     <Router>
-      <Dashboard path="/app/" />
+      <Dashboard path="/app/dashboard/" />
     </Router>
   )
 }
