3c5a6cab90a8126069d3f77b2721ebff8bf21170
Mike <mike@t2.ca>
2020-05-26 10:31:28 -0600

    fix

--- packages/www/src/components/dashloggedout.js
+++ packages/www/src/components/dashloggedout.js
@@ -5,20 +5,9 @@ import { jsx, Container, Flex, NavLink, Heading, Button } from 'theme-ui'
 import { gql, useQuery } from '@apollo/client'
 import { IdentityContext } from '../../identity-context'
 
-const GET_LIST = gql`
-  query GetList {
-    getList {
-      id
-      firstname
-      lastname
-      email
-    }
-  }
-`
-
 const DashLoggedOut = () => {
   const { user, identity: netlifyIdentity } = useContext(IdentityContext)
-  const { loading, error, data, refetch } = useQuery(GET_LIST)
+
   return (
     <Container>
       <Flex as="nav">
@@ -37,21 +26,6 @@ const DashLoggedOut = () => {
       <Flex sx={{ flexDirection: 'column', p: 3 }}>
         <Heading>hello world</Heading>
 
-        <Flex sx={{ flexDirection: 'column' }}>
-          {loading ? <div>loading...</div> : null}
-          {error ? <div>{error.message}</div> : null}
-          {!loading && !error && (
-            <ul sx={{ listStyleType: 'none' }}>
-              {data.getList.map((item) => (
-                <Flex key={item.id}>
-                  <span sx={{ color: `blue.7`, m: 2 }}>{item.firstname}</span>
-                  <span sx={{ color: `blue.4`, m: 2 }}>{item.lastname}</span>
-                  <span sx={{ fontWeight: 600, m: 2 }}>{item.email}</span>
-                </Flex>
-              ))}
-            </ul>
-          )}
-        </Flex>
         <Button
           sx={{ mt: 3 }}
           onClick={() => {
